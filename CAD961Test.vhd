ca311fd65044fa1de0bf2b7a99754f1fe229ec2e
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-10 18:33:58 +0430

    Add component VGA_board

--- CAD961Test.vhd
+++ CAD961Test.vhd
@@ -96,6 +96,23 @@ Component VGA_controller
   );
 end component;
 
+Component VGA_board
+	port ( CLK_50MHz		: in std_logic;	
+			RESET				: in std_logic;
+			key0,key1,key2,key3 : in std_logic;
+			ColorOut	: out std_logic_vector(11 downto 0); -- RED & GREEN & BLUE
+			SQUAREWIDTH		: in std_logic_vector(7 downto 0);
+			ScanlineX		: in std_logic_vector(10 downto 0);
+			player1score   : inout std_logic_vector(3 downto 0);
+			player2score   : inout std_logic_vector(3 downto 0);
+			ScanlineY		: in std_logic_vector(10 downto 0);
+			start_stop     : in std_logic;
+			speedMulti     : in std_logic_vector(3 downto 0);
+			single_double  : in std_logic;
+			level          : in std_logic
+  );
+end component;
+
 
 
 
