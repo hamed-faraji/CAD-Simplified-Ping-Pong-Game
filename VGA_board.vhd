f1101e627c170aa090656105027bfbd2a8df0ee5
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-10 18:42:36 +0430

    Add entity that we need

--- VGA_board.vhd
+++ VGA_board.vhd
@@ -3,3 +3,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+
+entity VGA_board is
+  port ( CLK_50MHz		: in std_logic;
+			RESET				: in std_logic;
+			key0,key1,key2,key3 : in std_logic;
+			ColorOut			: out std_logic_vector(11 downto 0); -- RED & GREEN & BLUE
+			SQUAREWIDTH		: in std_logic_vector(7 downto 0);
+			ScanlineX		: in std_logic_vector(10 downto 0);
+			player1score   : inout std_logic_vector(3 downto 0);
+			player2score   : inout std_logic_vector(3 downto 0);
+			ScanlineY		: in std_logic_vector(10 downto 0);
+			start_stop     : in std_logic;
+			speedMulti     : in std_logic_vector(3 downto 0);
+			single_double  : in std_logic;
+			level          : in std_logic
+  );
+end VGA_board;
+
+architecture Behavioral of VGA_board is
+
+
+begin
+
+
+
+
+
+end Behavioral;
