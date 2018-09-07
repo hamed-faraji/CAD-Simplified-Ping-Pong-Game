1cdc2d03e3ce3609678664ebf64deee23b44b55f
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-07 13:12:45 +0430

    Add entity to VGA_Square

---
+++ VGA_Square.vhd
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------------
+-- Moving Square Demonstration 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity VGA_Square is
+  port ( CLK_50MHz		: in std_logic;
+			RESET				: in std_logic;
+			ColorOut			: out std_logic_vector(11 downto 0); -- RED & GREEN & BLUE
+			SQUAREWIDTH		: in std_logic_vector(7 downto 0);
+			ScanlineX		: in std_logic_vector(10 downto 0);
+			ScanlineY		: in std_logic_vector(10 downto 0)
+  );
+end VGA_Square;
