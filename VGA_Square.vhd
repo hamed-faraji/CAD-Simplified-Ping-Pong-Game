8d280c62f30cdccd1da302cc7d62af15bb96e9f6
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-07 13:13:45 +0430

    Add some required signals

--- VGA_Square.vhd
+++ VGA_Square.vhd
@@ -16,3 +16,23 @@ entity VGA_Square is
 			ScanlineY		: in std_logic_vector(10 downto 0)
   );
 end VGA_Square;
+
+architecture Behavioral of VGA_Square is
+  
+  signal ColorOutput: std_logic_vector(11 downto 0);
+  
+  signal SquareX: std_logic_vector(9 downto 0) := "0000001110";  
+  signal SquareY: std_logic_vector(9 downto 0) := "0000010111";  
+  signal SquareXMoveDir, SquareYMoveDir: std_logic := '0';
+  --constant SquareWidth: std_logic_vector(4 downto 0) := "11001";
+  constant SquareXmin: std_logic_vector(9 downto 0) := "0000000001";
+  signal SquareXmax: std_logic_vector(9 downto 0); -- := "1010000000"-SquareWidth;
+  constant SquareYmin: std_logic_vector(9 downto 0) := "0000000001";
+  signal SquareYmax: std_logic_vector(9 downto 0); -- := "0111100000"-SquareWidth;
+  signal ColorSelect: std_logic_vector(2 downto 0) := "001";
+  signal Prescaler: std_logic_vector(30 downto 0) := (others => '0');
+
+begin
+
+end process PrescalerCounter; 
+end Behavioral;
